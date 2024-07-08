816b5d047a6f91473ae19dbf44ba80785a6e3dce
Sushobhit Srivastava <sushobhitofficial@gmail.com>
2024-07-08 06:53:31 +0530

    Add inventory - Responsive

--- src/Pages/AddInventory.js
+++ src/Pages/AddInventory.js
@@ -32,34 +32,36 @@ class AddInventory extends React.Component {
         const isDisabled = !(this.state.cost && this.state.date && this.state.roomType && this.state.roomsAvailable) || this.state.isLoading
 
         return (
-            <div className="col-md-3 col-12 m-auto mt-5">
-                <div>
-                    <div className="text-dark-o h1 mb-5">Add Inventory</div>
-                    <Input placeholder="Room Type" onChange={onChange} value={this.state.roomType} name="roomType" className="mb-4" />
-                    <Label className="mb-0">Date</Label>
-                    <Input placeholder="Date" onChange={onChange} value={this.state.roomType} name="roomType" type="date" className="mb-4" />
-                    <Label className="mb-0">Total Available Rooms</Label>
-                    <Input placeholder="Total available rooms" onChange={onChange} value={this.state.roomsAvailable} name="roomAvailable" className="mb-4" />
-                    <Label className="mb-0">Cost</Label>
-                    <InputGroup className="border rounded mb-4">
-                        <InputGroupText className="border-0">
-                            <i class="bi bi-currency-rupee"></i>
-                        </InputGroupText>
-                        <Input className="border-0" placeholder="Cost" onChange={onChange} value={this.state.cost} name="cost" />
-                    </InputGroup>
-                    <Label className="mb-0">Should the room be visible to customers?</Label>
-                    <Input onChange={onChange} name="isVisible" value={this.state.isVisible} type="select" className="mb-5">
-                        <option value="true">Yes</option>
-                        <option value="false">No</option>
-                    </Input>
-                    <Button disabled={isDisabled} onClick={onSubmit} className="px-3 bg-primary-o">
-                        {this.state.isLoading ? "Loading.." : "Create Inventory"}
-                    </Button>
-                    {this.state.message &&
-                        <Alert color={this.state.messageColor}>
-                            {this.state.message}
-                        </Alert>
-                    }
+            <div className="mx-3">
+                <div className="col-md-3 col-12 m-auto mt-5">
+                    <div>
+                        <div className="text-dark-o h1 mb-5">Add Inventory</div>
+                        <Input placeholder="Room Type" onChange={onChange} value={this.state.roomType} name="roomType" className="mb-4" />
+                        <Label className="mb-0">Date</Label>
+                        <Input placeholder="Date" onChange={onChange} value={this.state.roomType} name="roomType" type="date" className="mb-4" />
+                        <Label className="mb-0">Total Available Rooms</Label>
+                        <Input placeholder="Total available rooms" onChange={onChange} value={this.state.roomsAvailable} name="roomAvailable" className="mb-4" />
+                        <Label className="mb-0">Cost</Label>
+                        <InputGroup className="border rounded mb-4">
+                            <InputGroupText className="border-0">
+                                <i class="bi bi-currency-rupee"></i>
+                            </InputGroupText>
+                            <Input className="border-0" placeholder="Cost" onChange={onChange} value={this.state.cost} name="cost" />
+                        </InputGroup>
+                        <Label className="mb-0">Should the room be visible to customers?</Label>
+                        <Input onChange={onChange} name="isVisible" value={this.state.isVisible} type="select" className="mb-5">
+                            <option value="true">Yes</option>
+                            <option value="false">No</option>
+                        </Input>
+                        <Button disabled={isDisabled} onClick={onSubmit} className="px-3 bg-primary-o">
+                            {this.state.isLoading ? "Loading.." : "Create Inventory"}
+                        </Button>
+                        {this.state.message &&
+                            <Alert color={this.state.messageColor}>
+                                {this.state.message}
+                            </Alert>
+                        }
+                    </div>
                 </div>
             </div>
         )
